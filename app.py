97f2eb860c4197a504688346d415f34e07c0cbb2
Santiago Carvajal <115636793+Masteryayo14@users.noreply.github.com>
2025-06-01 20:35:43 -0500

    Update app.py
    xdxdxd

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
 
+echo "from flask import Flask\napp = Flask(__name__)\n\n@app.route('/')\ndef home():\n    return 'API activa'" > app.py
+git add app.py
+git commit -m "Endpoint raíz funcionando con Flask"
